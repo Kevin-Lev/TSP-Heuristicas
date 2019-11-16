453ca1d8bcfea8e90a9b43abdd4b35696e4be653
kevin.levrone <kevinlevrone.r@gmail.com>
2019-11-16 02:29:17 -0300

    Inserção Mais Próxima finalizada; Adição de testes da TSPLIB

---
+++ caixeiroViajante.py
@@ -0,0 +1,88 @@
+import numpy as np
+import tsplib95 as tsp
+
+
+def vertice_mais_proximo(ciclo, grafo):
+    menor_peso = 9999999
+    vertice_proximo = -1
+
+    for i in range(len(ciclo)):
+        for j in range(3, len(ciclo[i])):
+            for key, value in ciclo[i][j].items():
+                if value < menor_peso and value != 0:
+                        vertice_proximo = key
+                        menor_peso = value
+                        aux_delete_coluna = j
+    
+    ciclo = np.delete(ciclo, aux_delete_coluna, axis=1)
+
+    return vertice_proximo, ciclo
+
+
+def calcula_insercoes(grafo, vertices_ciclo, vertice_mais_proximo):
+    menor_custo_total = 9999999
+    posicao_menor_custo = -1
+
+    for i in range(len(vertices_ciclo) - 1):
+        custo_total = grafo[vertices_ciclo[i]][vertice_mais_proximo] + grafo[vertice_mais_proximo][vertices_ciclo[i+1]] - grafo[vertices_ciclo[i]][vertices_ciclo[i+1]]
+
+        if custo_total < menor_custo_total:
+            menor_custo_total = custo_total
+            posicao_menor_custo = i 
+
+    vertices_ciclo.insert(posicao_menor_custo + 1, vertice_mais_proximo)
+
+    return vertices_ciclo
+
+
+def insercao_mais_proxima(grafo, ciclo, vertices_restantes, vertices_ciclo):
+    while vertices_restantes > 0:
+        vertice_proximo, ciclo = vertice_mais_proximo(ciclo, grafo)
+        
+        vertices_ciclo = calcula_insercoes(grafo, vertices_ciclo, vertice_proximo)
+
+        vertices_restantes -= 1
+
+    return vertices_ciclo
+
+
+def main():
+
+    # grafo = np.array([[0, 1, 2, 7, 5], [1, 0, 3, 4, 3], [2, 3, 0, 5, 2], [7, 4, 5, 0, 3], [5, 3, 2, 3, 0]])
+
+    problema = tsp.load_problem('testes/ftv33.atsp')
+
+    matrix = np.array(problema._create_explicit_matrix().numbers)
+
+    grafo = matrix.reshape(-1, problema.dimension)
+
+    print("Grafo Conexo:")
+    print(grafo)
+
+    ciclo_inicial = grafo[0:3, :]
+    novo_ciclo_inicial = []
+    vertices_ciclo = [0, 1, 2, 0]
+
+    for i in range(len(ciclo_inicial)):
+        for j in range(len(ciclo_inicial[i])):
+            novo_ciclo_inicial.append({j : ciclo_inicial[i][j]})
+
+    novo_ciclo_inicial = np.array(novo_ciclo_inicial)
+    novo_ciclo_inicial = novo_ciclo_inicial.reshape(-1, problema.dimension)
+    print("Ciclo inicial: ")
+    print(novo_ciclo_inicial)
+    vertices_restantes = len(grafo) - 3
+    # print("Vértices restantes: ")
+    # print(vertices_restantes)
+
+
+    ciclo_final = insercao_mais_proxima(grafo, novo_ciclo_inicial, vertices_restantes, vertices_ciclo)
+
+    custo_total = 0
+
+    for i in range(len(ciclo_final) - 1):
+        custo_total += grafo[ciclo_final[i]][ciclo_final[i+1]] 
+
+    print("\nMenor custo segundo a inserção mais próxima: " + str(custo_total))
+
+main()
